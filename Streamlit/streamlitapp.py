287e69847ed88b1c992a78951a8756ba004f7074
garymanleydata <104717230+garymanleydata@users.noreply.github.com>
2022-05-25 21:26:27 +0100

    streamlit

--- Streamlit/streamlitapp.py
+++ Streamlit/streamlitapp.py
@@ -2,6 +2,8 @@
 import streamlit as st 
 import pandas as pd
 import snowflake.connector
+import plotly.express as px
+import datetime
 
 st.set_page_config(
     page_title="Strava Data Dashboard",
@@ -11,6 +13,16 @@ st.set_page_config(
 
 # dashboard title
 st.title("Gary Manley Strava Data Dashboard")
+st.markdown('Streamlit is **_really_ cool**.')
+
+today = datetime.date.today()
+prev_date = today + datetime.timedelta(days=-30)
+start_date = st.date_input('Start date', prev_date)
+end_date = st.date_input('End date', today)
+if start_date < end_date:
+    st.success('Start date: `%s`\n\nEnd date:`%s`' % (start_date, end_date))
+else:
+    st.error('Error: End date must fall after start date.')
 
 def init_connection():
     return snowflake.connector.connect(**st.secrets["snowflake"])
@@ -34,8 +46,19 @@ def run_query(query):
 query = ('SELECT ACTIVITY_DATE ,  sum(DISTANCE_KM) distance_km, sum(moving_time_minutes) move_minutes FROM STRAVA_ACTIVITIES_STAR_FACT WHERE "TYPE" IN (\'Run\',\'Walk\') GROUP BY  ACTIVITY_DATE order by ACTIVITY_DATE')
 cur = conn.cursor().execute(query)
 df = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
+#df2 = df
+df = df[df.ACTIVITY_DATE.between(start_date, end_date)]
 df = df.rename(columns={'ACTIVITY_DATE':'index'}).set_index('index')
 df
 
 st.write('This is a line_chart.')
 st.line_chart(df)
+
+st.write('This is a bar chart with plotly.')
+
+query = ('SELECT ACTIVITY_DATE , "TYPE",  sum(DISTANCE_KM) distance_km, sum(moving_time_minutes) move_minutes FROM STRAVA_ACTIVITIES_STAR_FACT WHERE "TYPE" IN (\'Run\',\'Walk\') GROUP BY  ACTIVITY_DATE , "TYPE" order by ACTIVITY_DATE')
+cur = conn.cursor().execute(query)
+df2 = pd.DataFrame.from_records(iter(cur), columns=[x[0] for x in cur.description])
+df2 = df2[df2.ACTIVITY_DATE.between(start_date, end_date)]
+fig = px.bar(df2, x='ACTIVITY_DATE', y='DISTANCE_KM', color = "TYPE")
+st.plotly_chart(fig, use_container_width=True, sharing="streamlit")
